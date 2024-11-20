77c953bdbb0556cc6d7924c6d594e413b346f9d5
Josh Cordova <cordova.joshuacody@gmail.com>
2024-11-19 20:29:51 -0600

    Console Math Assignment

---
+++ MathConsoleApp/MathConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input number");
+            string userInput = Console.ReadLine();
+            int intUserInput = Convert.ToInt32 (userInput);
+            int product = intUserInput * 50;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+
+        }
+    }
+}
