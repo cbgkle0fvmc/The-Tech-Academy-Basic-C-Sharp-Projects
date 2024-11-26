c8740fea778fd8e5fca9b70022f69388d9f8ab31
Josh Cordova <cordova.joshuacody@gmail.com>
2024-11-26 10:13:46 -0600

    Shipping Quote Calculation Corrections

--- shippingQuote/shippingQuote/Program.cs
+++ shippingQuote/shippingQuote/Program.cs
@@ -14,23 +14,31 @@ namespace shippingQuote
             Console.WriteLine("Please enter package weight.");
             int packageWeight = Convert.ToInt32(Console.ReadLine());
 
+
             if (packageWeight > 50)
             {
                 Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day");
             }
-           
 
-            Console.WriteLine("Please enter a package width.");
-            int packageWidth = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Please enter a package width.");
+           int packageWidth = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter a package height");
-            int packageHeight = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Please enter a package height");
+           int packageHeight = Convert.ToInt32(Console.ReadLine());
 
-            double shippingCost = packageHeight * packageWidth * packageWeight * packageWeight / 100;
+            if (packageHeight * packageWidth > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
 
-            Console.WriteLine("Your estimated total for shipping this package is: " + shippingCost.ToString("C"));
-            
-            Console.ReadLine();
+            else
+            {
+                double shippingCost = packageHeight * packageWidth * packageWeight * packageWeight / 100;
+
+                Console.WriteLine("Your estimated total for shipping this package is: " + shippingCost.ToString("C"));
+
+                Console.ReadLine();
+            }
         }
     }
 }
