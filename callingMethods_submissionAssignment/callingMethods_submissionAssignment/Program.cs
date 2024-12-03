bfa2fc8267c869469830438cafe05111ea821258
Josh Cordova <cordova.joshuacody@gmail.com>
2024-12-03 05:48:13 -0600

    calling methods submission assignment

---
+++ callingMethods_submissionAssignment/callingMethods_submissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods_submissionAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            integerOperations integerOperations = new integerOperations();
+            Console.WriteLine("What number do you want to do mathemic operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(integerOperations.additionOperation(userInput));
+            Console.WriteLine(integerOperations.multiplicationOperation(userInput));
+            Console.WriteLine(integerOperations.divisionOperation(userInput));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ callingMethods_submissionAssignment/callingMethods_submissionAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("callingMethods_submissionAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("callingMethods_submissionAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d96afa3-2cce-4648-ae95-0cea38b1f978")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
