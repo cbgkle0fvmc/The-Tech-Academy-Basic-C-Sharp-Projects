463232153db51c8a22c78adff62d167ebb4a408e
Josh Cordova <cordova.joshuacody@gmail.com>
2024-12-09 20:35:12 -0600

    method submission assignment

---
+++ method_submissionAssignment/method_submissionAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_submissionAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            math math = new math(); //instantiate
+            Console.WriteLine("Please input first integer.");
+            int intInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input second optional integer.");
+            int secondIntInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.operation(intInput, secondIntInput));
+
+            Console.ReadLine();
+
+        }
+    }
+}
