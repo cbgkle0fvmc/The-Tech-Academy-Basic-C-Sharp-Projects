30623910b90d80bfa830e5c7bc5e278e5ab02a85
Josh Cordova <cordova.joshuacody@gmail.com>
2024-12-12 17:13:22 -0600

    abstractClass_submissionAssignment

---
+++ abstractClass_submissionAssignment/abstractClass_submissionAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClass_submissionAssignment
+{
+    public class Employee : Person //Step 3. Create another class called employee and have it inherit from the Person class
+    {
+        public override void SayName() //Step 4. Implement the SayName() method inside of the Employee class.
+        {
+            base.SayName();
+        }
+        
+    }
+}
---
+++ abstractClass_submissionAssignment/abstractClass_submissionAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClass_submissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 5. Inside the Main() method, instantiate the Employee object with the 
+            //firstName "Sample" and lastName "Student". Call the SayName() method on the object
+
+            Employee employee1 = new Employee() { firstName="Sample", lastName="Student"};
+            employee1.SayName();
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
