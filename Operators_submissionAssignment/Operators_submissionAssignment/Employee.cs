b1d4f3b114f362674cca665587b3d433963e17af
Josh Cordova <cordova.joshuacody@gmail.com>
2024-12-16 16:20:58 -0600

    operators submission assignment upload

---
+++ Operators_submissionAssignment/Operators_submissionAssignment/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_submissionAssignment
+{
+    public class Employee //Step 1. Create an employee class with Id, FirstName and LastName properties.
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        //public List <Employee> Employees { get; set; }
+
+        /* Step 2. In the Employee class, overload the “==” operator so it checks if two Employee objects are equal 
+         * by comparing their Id property. Remember that comparison operators must be overloaded in pairs. */
+
+        //Line 24 of Program.cs : Console.writeline(employee1==employee2), if true,
+        //Line 22 of Employee.cs will execute and return bool of 
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id; //return statement specifies employee id property ==
+        }
+
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+
+        }
+
+
+
+    }
+}
---
+++ Operators_submissionAssignment/Operators_submissionAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_submissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /* In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their
+         * properties. Then compare the two Employee objects using the newly overloaded operators and display 
+         * the results. */
+        {
+            Employee employee = new Employee(); //Instantiate employee object
+            //employee.Employees = new List<Employee>(); //Instantiate employee list
+
+            //assign values to employee 1 properties
+            Employee employee1 = new Employee() { Id= 1, FirstName="Joshua", LastName="Cordova"};
+            //assign values to employee 2 properties
+            Employee employee2 = new Employee() { Id = 2, FirstName = "Alicia", LastName = "Cordova" };
+
+            Console.WriteLine(employee1 == employee2); //Call operator overload ==
+            /* Employee class referred to as employee in 'employee' in program.cs
+             * Line 20 Employee (Class) employee1 (new instance of Employee object)
+             * Line 24: employee1==employee2 calls bool operator overloading, executes bool employee1.id==employee2.id*/
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
