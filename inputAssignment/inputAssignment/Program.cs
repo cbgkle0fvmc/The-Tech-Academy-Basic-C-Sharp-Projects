68ddcf90e6e6ea4e229ffa2218e894b88c634b2a
Josh Cordova <cordova.joshuacody@gmail.com>
2024-12-19 13:49:51 -0600

    inputAssignment

---
+++ inputAssignment/inputAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+
+namespace inputAssignment
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input whole number."); //Step 1. Ask the user for a number.
+            string logUserInput = Console.ReadLine();
+            Console.WriteLine(logUserInput);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\cordo\Desktop\Log\inputAssignment.txt"))
+            {
+                file.WriteLine(logUserInput + "\n");
+            }
+        }
+    }
+}
