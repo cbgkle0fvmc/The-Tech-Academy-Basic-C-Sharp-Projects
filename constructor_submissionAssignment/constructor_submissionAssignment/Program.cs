565dbccbc5a45e87fd633fc002955cd6f155e02e
Josh Cordova <cordova.joshuacody@gmail.com>
2024-12-20 14:50:17 -0600

    constructor submission assignment

---
+++ constructor_submissionAssignment/constructor_submissionAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace constructor_submissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fName = ("Please enter your first name."); //Step 1. Create a const variable
+            Console.WriteLine(fName);
+
+            var lName = ("Please enter your last name.."); //Step 2. Create a variable using the keyword “var.”
+            Console.Write(lName);
+
+            twoConstructors person1 = new twoConstructors() { FName="Josh", Lname="Cordova"};
+
+            Console.WriteLine(person1);
+
+            Console.ReadLine();
+        }
+       
+
+    }
+    
+}
---
+++ constructor_submissionAssignment/constructor_submissionAssignment/twoConstructors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using constructor_submissionAssignment;
+using System.Xml.Linq;
+
+namespace constructor_submissionAssignment
+{
+    public class twoConstructors
+    {
+
+        public twoConstructors(string fname, string lname) : this(fname, fname + lname)
+        {
+
+        }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+        public string username { get; set; }
+    }
+    
+    
+
+    
+}
