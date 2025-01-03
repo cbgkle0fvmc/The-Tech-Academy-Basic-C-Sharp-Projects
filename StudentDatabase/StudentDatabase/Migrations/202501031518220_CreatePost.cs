b2569eca2f7eefeeddfb1db05d7c6b3c497c6517
Josh Cordova <151390365+cbgkle0fvmc@users.noreply.github.com>
2025-01-03 10:07:26 -0600

    studentdb

---
+++ StudentDatabase/StudentDatabase/Migrations/202501031518220_CreatePost.cs
@@ -0,0 +1,29 @@
+﻿namespace StudentDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        GPA = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public float GPA { get; set; }
+    }
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
